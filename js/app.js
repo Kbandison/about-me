8a93f944a73da7c15b92b09cc9d4d9d756c5fce3
Kevin Bandison <kbandison@gmail.com>
2022-07-14 09:50:21 -0400

    added sixth and sventh questions

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ let questionTwo = prompt('Am I an Army Veteran?').toLowerCase();
 if (questionTwo === 'y' || questionTwo === 'yes'){
   alert('You are wrong!  I am a Navy Veteran.');
 } else if(questionTwo === 'n' || questionTwo ==='no'){
-  alert(`You are right ${visitor} !! I\'m actually a Navy Veteran!`);
+  alert(`You are right ${visitor} !! I'm actually a Navy Veteran!`);
 } else{
   alert('Answer yes or no.');
 }
@@ -28,7 +28,7 @@ let questionThree = prompt('Am I a college graduate?').toLowerCase();
 
 if (questionThree === 'y' || questionThree === 'yes'){
   alert('You are wrong!  I\'m still taking some courses in Computer Science, but decided to take a coding bootcamp to fast track my way to a career.');
-} else if(questionThree === 'n' || questionThree ==='no'){
+}else if(questionThree === 'n' || questionThree ==='no'){
   alert('You are right!! I\'m still enrolled in a college, but took a break to take this amazing coding class!');
 } else{
   alert('Answer yes or no.');
@@ -38,7 +38,7 @@ let questionFour = prompt('Do I currently live i the states?').toLowerCase();
 
 if (questionFour === 'y' || questionFour === 'yes'){
   alert('You are correct! I live in the Atlanta area. ');
-} else if(questionFour === 'n' || questionFour ==='no'){
+}else if(questionFour === 'n' || questionFour ==='no'){
   alert('You are wrong!! I do live in the states, in the Atlanta area. ');
 } else{
   alert('Answer yes or no.');
@@ -48,7 +48,7 @@ let questionFive = prompt('Do I currently work for a Tech company?').toLowerCase
 
 if (questionFive === 'y' || questionFive === 'yes'){
   alert('You are correct! Kinda, as I technically work in the warehouse section of Best Buy.  If I become a web developer, that\'s the first place I would look for employment.');
-} else if(questionFive === 'n' || questionFive ==='no'){
+}else if(questionFive === 'n' || questionFive ==='no'){
   alert('You are wrong!! Kinda, as I technically work in the warehouse section of Best Buy.  If I become a web developer, that\'s the first place I would look for employment.');
 } else{
   alert('Answer yes or no.');
@@ -58,12 +58,32 @@ let questionSix = prompt('How old am I?');
 
 for(let i = 1; i < 4; i++){
   if (questionSix === 31){
-    questionSix = prompt('Correct!');
-    break;
+    questionSix = alert('Correct! My birthday is in May 1991!');
   }else if (questionSix < 31){
     questionSix = prompt('Too Low!');
   }else if (questionSix > 31)
     questionSix = prompt('Too High!');
 }
 
+alert('Correct!! I was born in May of 1991. ');
+
+let port = ['Greece', 'Bahrain', 'Dubai'];
+
+let correctAnswer = port[0].toUpperCase();
+let correctAnswer2 = port[1].toUpperCase();
+let correctAnswer3 = port[2].toUpperCase();
+
+let userGuess = prompt('Which were my favorite ports to visit while on deployment?\n Marseille\n Portugal\n  Greece\n  Bahrain\n  Dubai\n  Naples\n  Yokosuka\n').toUpperCase();
+
+for(let i = 0; i < 5; i++){
+  if (userGuess === correctAnswer){
+    userGuess = alert(`Good Job.  I enjoyed going to ${port[1]} and ${port[2]} as well.`);break;
+  }else if (userGuess === correctAnswer2){
+    userGuess = alert(`Good Job.  I enjoyed going to ${port[0]} and ${port[2]} as well.`);break;
+  }else if (userGuess === correctAnswer3){
+    userGuess = alert(`Good Job.  I enjoyed going to ${port[0]} and ${port[1]} as well.`);break;
+  }else if(userGuess !== correctAnswer){
+    userGuess = prompt('Wrong.  Try again!\n Marseille\n Portugal\n  Greece\n  Bahrain\n  Dubai\n  Naples\n  Yokosuka\n').toUpperCase();
+  }
+}
 alert(`Thanks for playing my game ${visitor}!  Continue on to learn more about me!`);
